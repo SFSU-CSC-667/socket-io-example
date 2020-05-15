71fbaf08c7a97d01a898832907926555da07dc87
John Roberts <roberts.john@gmail.com>
2020-05-14 18:01:24 -0700

    Added post routes to demo use of io in routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
-const express = require( 'express' )
+const express = require('express')
+const { USER_JOINED, MESSAGE_SEND } = require('../src/constants/events')
+
 const router = express.Router()
 
-router.get( '/', ( request, response ) => {
-  response.render( 'index' )
+router.get('/', (request, response) => {
+  response.render('index')
+})
+
+router.post('/force-join/:name', (request, response) => {
+  const io = request.app.get('io')
+  const { name } = request.params
+
+  io.emit(USER_JOINED, { user: name, timestamp: Date.now() })
+  response.json({ ok: true })
+})
+
+router.post('/force-message/:message', (request, response) => {
+  const io = request.app.get('io')
+  const { message } = request.params
+
+  io.emit(MESSAGE_SEND, { user: 'anonymous', timestamp: Date.now(), message })
+  response.json({ ok: true })
 })
 
 module.exports = router
